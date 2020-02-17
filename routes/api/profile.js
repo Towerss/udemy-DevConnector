2f31249bcda41302644fca99c7c41c67691a98cf
@dtorres3 <dtorres@unifywebsolutions.com>
2020-02-18 08:51:01 +1000

    180220200850

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,30 @@
+'use strict';
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 /*
-@route  GET api/users
-@desc   Test route
-@access Public
+@route  GET api/profile/mine
+@desc   Get current user profile
+@access Private
 */
-router.get('/', (req, res) => {
-    res.send('Profile route')
+router.get('/mine', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id}).populate('user', ['name', 'avatar']);
+
+        if(!profile){
+            return res.status(400).json({ message: 'There is no profile for this user'})
+        }
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error at api/profile/mine');
+    }
 });
 
 module.exports = router;
