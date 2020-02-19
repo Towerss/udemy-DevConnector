52ff98421e522167d66d892244bc522f22a093d7
@dtorres3 <dtorres@unifywebsolutions.com>
2020-02-19 16:03:18 +1000

    190220201403

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,14 +95,19 @@ router.post('/', [auth, [
             try {
                 profile = await Profile.findOneAndUpdate(
                     {user: req.user.id},
-                    {$set: profile},
-                    {new: true}, (err) => { console.log("Try update error:" + err); }
+                    {$set: profileFields},
+                    {new: true}, (err, doc) => { 
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            return res.status(200).json(doc)
+                        }
+                    }
                 );
             } catch (error) {
-                console.log("Error inside profile update");
                 console.log(error.message);
             }
-            return res.status(200).json(profile);
+
         } else {
             // Create
             profile = new Profile(profileFields);
@@ -119,6 +124,57 @@ router.post('/', [auth, [
 
 
 
+});
+
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+    try {
+      const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+      res.json(profiles);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+      const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+  
+      if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+  
+      res.json(profile);
+    } catch (err) {
+      //console.error(err.message);
+      if (err.kind == 'ObjectId') {
+        return res.status(400).json({ msg: 'Profile not found' });
+      }
+      res.status(500).send('Server Error');
+    }
+});
+
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+    try {
+      // Remove user posts
+      await Post.deleteMany({ user: req.user.id });
+      // Remove profile
+      await Profile.findOneAndRemove({ user: req.user.id });
+      // Remove user
+      await User.findOneAndRemove({ _id: req.user.id });
+  
+      res.json({ msg: 'User deleted' });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
 });
 
 
